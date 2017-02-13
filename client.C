9655e070c0572dfdb8034461e44639c32c290827
ryanhillnv <ryanhill@tamu.edu>
2017-02-13 14:16:08 -0600

    Update client.C

--- client.C
+++ client.C
@@ -22,50 +22,60 @@
 int main (int argc, char **argv) {
 	int sockfd;
 	struct sockaddr_in servaddr;
-	struct sockaddr;
 	char cli_msg[1024];
 	char buffer[1024];
+	FILE * fp;
 	
 	if (argc != 2) {
 		printf("Error: Address currently in use.\n");
+		return(-1);
 	}
 	
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	
 	if (sockfd < 0) {
 		printf("Error: Could not open socket.\n");
+		return(-1);
 	}
 	
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT);
+	servaddr.sin_port = htons(12);
 	bzero(&servaddr, sizeof(servaddr));
 	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 	
-	if (connect(sockfd, (sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+	if (connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
 		printf("Error: Socket is closed.\n");
+		return(-1);
 	}
 	else {
-			connect(sockfd, (sockaddr *) &servaddr, sizeof(servaddr));
+			connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
 	}
 	
 	bzero(buffer, 1024);
 	
-	while(fgets(cli_msg, 1024, FILE *fp) != NULL) {
+	 if (fp == NULL) {
+		 printf("Error: Could not open file.\n");
+		 return(-1);
+	}
+	
+	while(fgets(cli_msg, 1024, fp) != NULL) {
 		if (write(sockfd, buffer, strlen(buffer)) < 0) {
 			printf("Error: Could not write to Socket.\n");
+			return(-1);
 		}
-		else write(sockfd, buffer, strlen(buffer))
+		else write(sockfd, buffer, strlen(buffer));
 		
 		bzero(buffer, 1024);
 		
 		if (read(sockfd, buffer, 1024) < 0) {
 			printf("Error: Could not read from Socket.\n");
+			return(-1);
 		}
 		else read(sockfd, buffer, 1024);
 		
 		printf("Echo received: %s", buffer);
 	}
 	
-	close(sockfd);
+	fclose(sockfd);
 	exit(0);
 }
