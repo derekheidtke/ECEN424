07b9248a4a95cdce17d417b505ff9a075664036d
ryanhillnv <ryanhill@tamu.edu>
2017-02-13 15:52:36 -0600

    Update client.C

--- client.C
+++ client.C
@@ -25,58 +25,62 @@ int main (int argc, char **argv) {
 	char buffer[1024];
 	
 	if (argc != 3) {
-		printf("Error: Input IPv4 address then socket number surrounded individually by quotations after run argument\n");
+		perror("Error: Input IPv4 address then socket number surrounded individually by quotations after run argument\n");
 		return(-1);
-	}
+	} //The running of this program should include two arguments, the IPv4 Address and the Port #. If there are not two arguments, the error is printed.
 	
-	servport = atoi(argv[2]);
+	servport = atoi(argv[2]); //Store the second input argument into servport as an integer.
+	printf("Server Port is %d\n", servport);
 	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); //Declare socket.
 	
 	if (sockfd < 0) {
-		printf("Error: Could not open socket.\n");
+		perror("Error: Could not open socket.\n");
 		return(-1);
-	}
+	} // sockfd returns negative if there is an error.
 	
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(servport);
-	bzero(&servaddr, sizeof(servaddr));
-	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	servaddr.sin_port = htons(servport); //Declaring server properties.
+	bzero(&servaddr, sizeof(servaddr)); //Initializing servaddr to zero.
+	
+	inet_pton(AF_INET, argv[1], &(servaddr.sin_addr)); //Takes input text argument and stores it in servaddr under the AF_INET format
+	
+	printf("%s\n", argv[1]);
 	
 	if (&servaddr.sin_addr <= 0) {
-		printf("Error: Invalid Address.\n");
+		perror("Error: Invalid Address.\n");
 		return(-1);
-	}
+	} //Check if address was entered correctly.
 	
 	if (connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
-		printf("Error: Socket is closed.\n");
+		perror("Error: Socket is closed.\n");
 		return(-1);
-	}
+	} //Test if connection can be established.
 	else {
 			connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
-	}
+	} //Establish connection
 	
-	bzero(buffer, 1024);
+	bzero(buffer, 1024); //Initialize buffer to zero.
 	
-	while(fgets(buffer, 1024, stdin) != NULL) {
+	while(fgets(buffer, 1024, stdin) != NULL) { //Read from command line to the buffer continuously in 1024 segments or until an endline argument.
 		
 		if (write(sockfd, buffer, strlen(buffer)) < 0) {
-			printf("Error: Could not write to Socket.\n");
+			perror("Error: Could not write to Socket.\n");
 			return(-1);
-		}
-		else write(sockfd, buffer, strlen(buffer));
+		} //Check for errors in writing to socket.
+		else write(sockfd, buffer, strlen(buffer)); //Write from buffer to socket.
 		
-		bzero(buffer, 1024);
+		bzero(buffer, 1024); //Flush the buffer.
 		
 		if (read(sockfd, buffer, 1024) < 0) {
-			printf("Error: Could not read from Socket.\n");
+			perror("Error: Could not read from Socket.\n");
 			return(-1);
-		}
-		else read(sockfd, buffer, 1024);
+		} //Check for errors in reading server echo.
+		else read(sockfd, buffer, 1024); //Read from sockfd into buffer.
 		
-		printf("Echo received: %s", buffer);
+		printf("Echo received: %s", buffer); //Print the server echo into terminal.
 	}
 	
-	close(sockfd);
+	close(sockfd); //Close socket and exit program.
 	exit(0);
 }
