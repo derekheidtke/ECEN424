34c1acf83d7f01490e5519c6e9ec29eb90136c2a
derekheidtke <derek.heidtke@tamu.edu>
2017-04-21 15:25:04 -0500

    Successful construction of data packets.

--- prog3/server.C
+++ prog3/server.C
@@ -18,12 +18,12 @@
 
 int main (int argc, char** argv) {
 
-	const int MAXLINE = 512;
-
 	int 	servport, sockfd;
-	struct sockaddr_in servaddr, cliaddr;
+	struct 	sockaddr_in servaddr, cliaddr;
 
-	char* servAddrIP;
+	char* 	servAddrIP;
+
+	pid_t 	child_pid;
 
 
 	// Get port number from command line
@@ -37,15 +37,15 @@ int main (int argc, char** argv) {
 	
 
 	// Create socket
-	if ( (sockfd = socket(AF_INET,SOCK_DGRAM,0)) < 0 ){
+	if ( (sockfd = socket(PF_INET,SOCK_DGRAM,0)) < 0 ){
 		std::cerr << "\nERROR: " << std::flush;
 		std::cout << strerror(errno) << std::endl;
 		return 0;
 	}
-	// std::cout << "Socket created successfully." << std::endl;
+	std::cout << "Parent socket created successfully." << std::endl;
 
 	bzero(&servaddr,sizeof(servaddr));
-	servaddr.sin_family			= AF_INET;				// use UDP datagram Protocol
+	servaddr.sin_family			= PF_INET;				// use UDP datagram Protocol
 	servaddr.sin_addr.s_addr	= htonl(INADDR_ANY);	// use any interface
 	servaddr.sin_port			= htons(servport);		// use portno provided by user
 
@@ -55,39 +55,108 @@ int main (int argc, char** argv) {
 		std::cout << strerror(errno) << std::endl;
 		return 0;
 	}
-	// std::cout << "Bind successful." << std::endl;
+	std::cout << "Parent bind successful." << std::endl;
 
 	for (;;){
 
 		int			n;
-		socklen_t	len;
+		socklen_t	len = sizeof(cliaddr);
 		char		mesg[MAXLINE];
 		TFTPMESSAGE	message;
 
-		n = recvfrom(sockfd, mesg, MAXLINE, 0, (struct sockaddr*) &cliaddr, &len);
+		// blocking read of next datagram
+		n = recvfrom(sockfd, (void*)mesg, (size_t)MAXLINE, 0, (struct sockaddr*) &cliaddr, &len);
 		if (n == -1){
-			std::cerr << "\nERROR: " << std::flush;
+			std::cerr << "\nERROR: " << len << " " << std::flush;
 			std::cout << strerror(errno) << std::endl;
+			return 0;
 		}
 
 		tftpDecode(mesg, &message);
 
-		std::cout << "\nopcode: " << message.opcode << std::flush;
-		std::cout << "\nseqNum: " << message.seqNum << std::flush;
-		std::cout << "\nerror: "  << message.error  << std::flush;
-		std::cout << "\nfilename: " << message.filename << std::flush;
+		// std::cout << "\nopcode: " << message.opcode << std::flush;
+		// std::cout << "\nseqNum: " << message.seqNum << std::flush;
+		// std::cout << "\nerror: "  << message.error  << std::flush;
+		// std::cout << "\nfilename: " << message.filename << std::flush;
 
-		std::cout << "\n" << n << std::flush;
-		std::cout << "\n" << mesg << std::flush;
+		// ====================================================================================
 
-		// fork process to handle new client
+		// fork process to handle new RRQ request
+		child_pid = fork();
+		if ( child_pid < 0 ){
+			std::cout << strerror(errno) << std::endl;
+			return 0;
+		}
+		if (child_pid == 0) { // child process: handle RRQ request
+
+			// if decoded message is RRQ
+			if (message.opcode == TFTP_RRQ) {
+
+				int 	newSockfd;
+				struct 	sockaddr_in childaddr;
+
+				// close existing socket descriptor from parent
+				if ( (close(sockfd)) != 0 ) {
+					std::cout << strerror(errno) << std::endl;
+				}
+
+				// bind new ephemeral socket
+				if ( (newSockfd = socket(PF_INET,SOCK_DGRAM,0)) < 0 ){
+					std::cerr << "\nERROR: " << std::flush;
+					std::cout << strerror(errno) << std::endl;
+					return 0;
+				}
+				bzero(&servaddr,sizeof(servaddr));
+				servaddr.sin_family			= PF_INET;				// use UDP datagram Protocol
+				servaddr.sin_addr.s_addr	= htonl(INADDR_ANY);	// use any interface
+				servaddr.sin_port			= htons(0);				// use ephemeral port
+
+				// Bind socket to port/IP address
+				if ( (bind(newSockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) ) != 0 ){
+					std::cerr << "\nERROR: " << std::flush;
+					std::cout << strerror(errno) << std::endl;
+					return 0;
+				}
+				std::cout << "\nChild socket created successfully." << std::flush;
+
+				// send data file to client
+				CLIRECORD clientInfo;
+				strcpy(clientInfo.filename, message.filename);
+				clientInfo.seqNum 			= 0;
+				clientInfo.lastSegmentSent 	= 0;
+				clientInfo.cliaddr 			= cliaddr;
+				clientInfo.cliSockfd 		= newSockfd;
+
+				tftpRespondRRQ(&message, (struct sockaddr*) &cliaddr, &clientInfo);
+
+				while(clientInfo.lastSegmentSent != 1) {
+					n = recvfrom(newSockfd, mesg, MAXLINE, 0, (struct sockaddr*) &cliaddr, &len);
+					tftpDecode(mesg, &message);
+
+					if (message.opcode == TFTP_ACK) {
+						std::cout << "\nWe got an ACK ... :| " << std::flush;
+
+						tftpRespondACK(&message, (struct sockaddr*)&cliaddr, &clientInfo);
+
+						
+					} else if (message.opcode == TFTP_ERR) {
+						std::cout << "\nWe got an ERR ... :| " << std::flush;
+					} else if (message.opcode == TFTP_RRQ) {
+						std::cout << "\nWe got a redundant RRQ ... :| " << std::flush;
+					} else {
+						std::cerr << "\nERROR: Unknown TFTP message type." << std::flush;
+						return -1;
+					}
+				}
 
 
-		// server child: recvfrom()
+				std::cout << "\nChild closing." << std::flush;
+				return 0;
 
-		// processing
+			} 
+		} // child block
 
-		// server child: sendto()
+
 
 
 	} // forever loop
